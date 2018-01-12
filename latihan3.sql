271d92529c83d0ef07fac318d55d85a07fd858c4
iqbalsalman <iqbalsalman1707@gmail.com>
2018-01-12 09:46:03 +0700

    DBML insert update select

---
+++ latihan3.sql
@@ -0,0 +1,14 @@
+insert into countries values('ID','Indonesia',3)
+
+insert into locations values(6232,'cinunuk','40526','Kab. Bandung','Ja5a Barat','ID');
+insert into locations values(6231,'Ujung Berung','40521','Kab. Bandung','Jawa Barat','ID');	
+insert into locations values(6233,'Margahayu Raya','40625','Kab. Bandung','Jawa Barat','ID');
+insert into locations values(6230,'Blok M','40620','Jakarta Selatan','DKI Jakarta','ID');
+insert into locations values(6220,'Slipi','40521','Jakarta Utara','DKI Jakarta','ID');
+
+
+update employees set commission_pct='0.10'
+where job_id='IT_PROG'
+
+	
+
