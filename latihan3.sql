ed57829db0682143a0a52e51bc5f012c260860be
iqbalsalman <iqbalsalman1707gmail.comgit config --global user.email iqbalsalman1707gmail.com>
2018-01-16 14:54:35 +0000

    Latihan SQL

--- latihan3.sql
+++ latihan3.sql
@@ -1,5 +1,7 @@
+--1
 insert into countries values('ID','Indonesia',3)
 
+--2
 insert into locations values(6232,'cinunuk','40526','Kab. Bandung','Ja5a Barat','ID');
 insert into locations values(6231,'Ujung Berung','40521','Kab. Bandung','Jawa Barat','ID');	
 insert into locations values(6233,'Margahayu Raya','40625','Kab. Bandung','Jawa Barat','ID');
@@ -7,18 +9,19 @@ insert into locations values(6230,'Blok M','40620','Jakarta Selatan','DKI Jakart
 insert into locations values(6220,'Slipi','40521','Jakarta Utara','DKI Jakarta','ID');
 
 
+--3
 update employees set commission_pct='0.10'
-where job_id='IT_PROG'
+where job_id='IT_PROG' and employee_id(104)
 
 
-
+--4
 	create table karyawan_tdi(
      kode_karyawan INTEGER  PRIMARY KEY NOT NULL,
-     nama_lengkap varchar(50),
-     alamat_rumah INTEGER,
-     alamat_domisil INTEGER,
-     jabatan varchar(10),
-     bagian  INTEGER
+     nama_lengkap varchar(50) not null,
+     alamat_rumah INTEGER not null,  
+     alamat_domisil INTEGER not null,
+     jabatan varchar(10) not null,
+     bagian  INTEGER not null
 		);
 
 
@@ -46,22 +49,7 @@ ADD CONSTRAINT fk_Karyawan_tdi_bagian FOREIGN KEY (bagian)
 REFERENCES departments(department_id) ON UPDATE CASCADE ON DELETE CASCADE;
 
 
-
-
-select k.kode_karyawan,nama_lengkap,l.street_address||' ,'||l.city AS "Alamat Rumah"
-       ,l.street_address||' ,'||l.city AS "Alamat Domisil",d.department_name,j.job_title
-from 
-       karyawan_tdi k 
-       join locations l on (k.alamat_rumah=l.location_id)
-       join jobs j on (k.jabatan=job_id)
-       join departments d on (k.bagian=department_id)
-       where k.alamat_rumah=l.location_id 
-       and l.location_id=alamat_domisili
-
-
-
-
-
+--5
 
 insert into  karyawan_tdi values(2,'Dimas Maryanto',6232,6230,'IT_PROG',60);
 insert into karyawan_tdi values(3,'Hari Sapto',6233,6233,'IT_PROG',60);
@@ -69,6 +57,7 @@ insert into karyawan_tdi values(4,'Hari Sapto Adi',6220,6220,'IT_PROG',60);
 insert into karyawan_tdi values(5,'Arip Permana',6233,6233,'AD_PRES',90);
 insert into karyawan_tdi values(6,'Zera',6233,6233,'HR_REP',10);
 
+--6
 
 select k.kode_karyawan,nama_lengkap,l.street_address||' ,'||l.city AS "Alamat Rumah"
        ,l.street_address||' ,'||l.city AS "Alamat Domisil",d.department_name,j.job_title
@@ -77,21 +66,28 @@ from
        inner join locations l on (k.alamat_rumah=l.location_id)
        inner join jobs j on (k.jabatan=job_id)
        inner join departments d on (k.bagian=department_id)
-       where k.alamat_rumah=l.location_id 
-       and l.location_id=alamat_domisili
-       and j.job_id=k.jabatan
-       and d.department_id=k.bagian
 
-select
-      k.nama_lengkap,e.first_name||' '||e.last_name as "Nama",
-      j.job_title,l.street_address||' ,'||l.city AS "cocat",
-      d.department_name
+--7
+
+select k.nama_lengkap,l.street_address||' ,'||l.city as alamat_lengkap
+       ,d.department_name,j.job_title
 from 
-      karyawan_tdi k join jobs j on (k.jabatan=j.job_id)
-      join locations l on (k.alamat_rumah=l.location_id)
-      join employees e on (j.job_id=e.job_id)
-      join departments d on (e.department_id=e.department_id)
-      where e.job_id='IT_PROG'
+       karyawan_tdi k 
+       inner join locations l on (k.alamat_rumah=l.location_id)
+       inner join jobs j on (k.jabatan=job_id)
+       inner join departments d on (k.bagian=department_id)
+       where k.jabatan='IT_PROG' 
+       union all
+       select e.first_name||' '|| e.last_name nama_lengkap,l.street_address||' ,'||l.city as alamat_lengkap
+       ,d.department_name,j.job_title
+      
+from 
+       employees e 
+       inner join jobs j on (j.job_id=e.job_id) 
+       inner join departments d on (e.department_id=d.department_id)
+       inner join locations l on (d.location_id=l.location_id)
+       where j.job_id='IT_PROG'
+       order by alamat_lengkap desc
 
 
 
